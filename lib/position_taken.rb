7c394ac4987bf35a34784c29fea73f260a42580a
concurrent-server-0630 <andycruz008@gmail.com>
2020-01-12 03:58:12 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 def position_taken?(board, index)
-  if board[index] == ("X" || "O")
+  if board[index] == "X"  
    true
+   elsif board[index] == "X"  
    elsif board[index] == (" " || "")
    false
  else
